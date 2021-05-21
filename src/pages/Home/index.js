e45a27f4fbb19ba8a8a33014cd68d97c739e7160
Lorena <lorena@proesc.com>
2021-05-21 00:57:24 -0300

    Adicionada routas e estrutura inicial da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,1 +1,8 @@
-Adicionar
+import React from 'react';
+import './styles.css';
+
+export default function Home(){
+    return(
+        <h1>Oi</h1>
+    )
+}
--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,14 @@
-Adicionar
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import Home from './pages/Home';
+
+function Routes(){
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
