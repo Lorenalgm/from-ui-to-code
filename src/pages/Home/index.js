f68c659256891d1e927e8c1b8173c87571af5a95
Lorena <lorena@proesc.com>
2021-05-21 01:09:01 -0300

    Criado jsx da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './styles.css';
+import logo from '../../assets/logo.png';
+import Card from '../../componets/Card';
 
 export default function Home(){
     return(
-        <h1>Oi</h1>
+        <div className="home-container">
+            <header>
+                <img src={logo} alt="Devxtalks" />
+            </header>
+            <main>
+                <h1>Próximos eventos</h1>
+                <div className="events list">
+                    <Card />
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </main>
+        </div>
     )
 }
